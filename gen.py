ff87cd2c69b8f4fe56489f0cf65c931f577c8a73
vazh0310 <60910587+vazh0310@users.noreply.github.com>
2021-10-30 23:48:47 +0500

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,12 +1,33 @@
+#!/usr/bin/env python3
+"""Password generator."""
+# -*- coding: UTF-8 -*-
 import random
+import string
 
-chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-number = input('количество паролей?'+ "\n")
-length = input('длина пароля?'+ "\n")
-number = int(number)
-length = int(length)
-for n in range(number):
-    password =''
+
+def gen(length=8, method=["lowercase", "uppercase", "digits", "punctuation"]):
+    """String.lowercase, uppercase, digits, punctuation."""
+    pwd = []
     for i in range(length):
-        password += random.choice(chars)
-    print(password)
+        choice = random.choice(method)
+        if choice == "lowercase":
+            pwd.append(random.choice(string.ascii_lowercase))
+        if choice == "uppercase":
+            pwd.append(random.choice(string.ascii_uppercase))
+        if choice == "digits":
+            pwd.append(random.choice(string.digits))
+        if choice == "punctuation":
+            pwd.append(random.choice(string.punctuation))
+        if choice == "string":
+            pwd.append(random.choice(string.punctuation))
+
+    random.shuffle(pwd)
+    return ''.join(pwd)
+
+# Examples
+print (gen())  # /79](g}7L
+print (gen(method=["uppercase"]))  # RSLCHWYED
+print (gen(method=["digits"]))  # 894157281
+print (gen(method=["punctuation"]))  # ~|{-()_**
+print (gen(method=["uppercase", "lowercase", "digits"]))  # 0ceA711iq
+print (gen(length=15, method=["uppercase", "lowercase", "digits"]))  # 97A3M481njJs1H3B
